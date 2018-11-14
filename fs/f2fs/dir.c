0ca0df9b675e3264e040c15cc289808114b96893
Jaegeuk Kim <jaegeuk@kernel.org>
2018-11-14 12:40:30 -0800

    f2fs: check memory boundary by insane namelen
    [ Upstream commit 4e240d1bab1ead280ddf5eb05058dba6bbd57d10 ]
    
    If namelen is corrupted to have very long value, fill_dentries can copy
    wrong memory area.
    
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -1016,6 +1016,7 @@ int f2fs_fill_dentries(struct dir_context *ctx, struct f2fs_dentry_ptr *d,
 
 		/* check memory boundary before moving forward */
 		bit_pos += GET_DENTRY_SLOTS(le16_to_cpu(de->name_len));
+<<<<<<< HEAD
 		if (unlikely(bit_pos > d->max ||
 				le16_to_cpu(de->name_len) > F2FS_NAME_LEN)) {
 			f2fs_warn(sbi, "%s: corrupted namelen=%d, run fsck to fix.",
@@ -1026,6 +1027,17 @@ int f2fs_fill_dentries(struct dir_context *ctx, struct f2fs_dentry_ptr *d,
 		}
 
 		if (IS_ENCRYPTED(d->inode)) {
+=======
+		if (unlikely(bit_pos > d->max)) {
+			f2fs_msg(F2FS_I_SB(d->inode)->sb, KERN_WARNING,
+				"%s: corrupted namelen=%d, run fsck to fix.",
+				__func__, le16_to_cpu(de->name_len));
+			set_sbi_flag(F2FS_I_SB(d->inode)->sb->s_fs_info, SBI_NEED_FSCK);
+			return -EINVAL;
+		}
+
+		if (f2fs_encrypted_inode(d->inode)) {
+>>>>>>> bc08d46f010c (f2fs: check memory boundary by insane namelen)
 			int save_len = fstr->len;
 
 			err = fscrypt_fname_disk_to_usr(d->inode,
