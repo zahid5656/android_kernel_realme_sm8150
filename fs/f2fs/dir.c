1344d967bbfce18e641b69246f8f96a6283cbaeb
Sheng Yong <shengyong1@huawei.com>
2019-01-07 15:02:34 +0800

    fsfs

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -1017,6 +1017,7 @@ int f2fs_fill_dentries(struct dir_context *ctx, struct f2fs_dentry_ptr *d,
 		/* check memory boundary before moving forward */
 		bit_pos += GET_DENTRY_SLOTS(le16_to_cpu(de->name_len));
 <<<<<<< HEAD
+<<<<<<< HEAD
 		if (unlikely(bit_pos > d->max ||
 				le16_to_cpu(de->name_len) > F2FS_NAME_LEN)) {
 			f2fs_warn(sbi, "%s: corrupted namelen=%d, run fsck to fix.",
@@ -1029,6 +1030,10 @@ int f2fs_fill_dentries(struct dir_context *ctx, struct f2fs_dentry_ptr *d,
 		if (IS_ENCRYPTED(d->inode)) {
 =======
 		if (unlikely(bit_pos > d->max)) {
+=======
+		if (unlikely(bit_pos > d->max ||
+				le16_to_cpu(de->name_len) > F2FS_NAME_LEN)) {
+>>>>>>> 6a27f426266a (f2fs: check if file namelen exceeds max value)
 			f2fs_msg(F2FS_I_SB(d->inode)->sb, KERN_WARNING,
 				"%s: corrupted namelen=%d, run fsck to fix.",
 				__func__, le16_to_cpu(de->name_len));
